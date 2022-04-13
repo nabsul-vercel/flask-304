58eeefa42a908176482797f69e8ece2369918301
Nabeel Sulieman <nabsul@outlook.com>
2022-04-13 11:12:47 -0700

    list files

--- api/app.py
+++ api/app.py
@@ -1,4 +1,11 @@
 from flask import Flask
+import os
 app = Flask(__name__)
+
+@app.route('/files')
+def show_files():
+    return os.popen('ls -la').read()
+
 if __name__ == '__main__':
     app.run(host='localhost', port=8080)
+
